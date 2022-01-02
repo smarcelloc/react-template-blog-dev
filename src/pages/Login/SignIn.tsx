ebae9d77f291a6eb54b0d29f6fb1558613977224
Marcello Silvério <smarcelloc@gmail.com>
2022-01-02 10:11:50 -0300

    feat: add formik for page Sign In

--- src/pages/Login/SignIn.tsx
+++ src/pages/Login/SignIn.tsx
@@ -6,83 +6,93 @@ import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+
+import { Formik, FormikProps } from 'formik';
 
 import MyLink from '../../components/Link';
 import accountAction from '../../store/actions/accountAction';
-import { isRequestError } from '../../utils/request';
+
+interface FormValues {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [email, setEmail] = React.useState('john@example.com');
-  const [password, setPassword] = React.useState('password@123');
-  const [messageError, setMessageError] = React.useState('');
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    try {
-      event.preventDefault();
-      await dispatch(accountAction.signIn(email, password));
-      navigate('/');
-    } catch (error: any) {
-      if (isRequestError(error)) {
-        setMessageError(error.response.data.message);
-      } else {
-        setMessageError(error.message);
-      }
-    }
+  const initialValues: FormValues = {
+    email: 'john@example.com',
+    password: 'password@123',
   };
 
   return (
     <>
-      <Typography color="error.main">{messageError}</Typography>
-      <form>
-        <TextField
-          id="email"
-          name="email"
-          autoComplete="email"
-          label="Email"
-          variant="outlined"
-          fullWidth
-          autoFocus
-          required
-          type="email"
-          margin="normal"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values, { setStatus, setSubmitting }) => {
+          try {
+            await dispatch(accountAction.signIn(values.email, values.password));
+            navigate('/');
+          } catch (error: any) {
+            setStatus({ success: false });
+            setSubmitting(false);
+          }
+        }}>
+        {({
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+          values,
+        }: FormikProps<FormValues>) => (
+          <form noValidate onSubmit={handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              autoComplete="email"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              autoFocus
+              type="email"
+              margin="normal"
+              value={values.email}
+              onChange={handleChange}
+            />
 
-        <TextField
-          id="password"
-          name="password"
-          label="Password"
-          autoComplete="password"
-          variant="outlined"
-          fullWidth
-          required
-          type="password"
-          margin="normal"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              autoComplete="password"
+              variant="outlined"
+              fullWidth
+              type="password"
+              margin="normal"
+              value={values.password}
+              onChange={handleChange}
+            />
 
-        <FormControlLabel
-          control={<Checkbox defaultChecked id="remember" name="remember" />}
-          label="Remember me"
-        />
+            <FormControlLabel
+              control={
+                <Checkbox defaultChecked id="remember" name="remember" />
+              }
+              label="Remember me"
+            />
 
-        <Button
-          fullWidth
-          variant="contained"
-          color="primary"
-          type="submit"
-          size="large"
-          sx={{ marginY: 3 }}
-          onClick={handleSubmit}>
-          Sign In
-        </Button>
-      </form>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              type="submit"
+              size="large"
+              disabled={isSubmitting}
+              sx={{ marginY: 3 }}>
+              Sign In
+            </Button>
+          </form>
+        )}
+      </Formik>
 
       <MyLink to="/login/register">Create a account</MyLink>
     </>
