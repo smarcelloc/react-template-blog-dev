12db75f50255135c5a6613fea5fde53c880dec42
Marcello Silvério <smarcelloc@gmail.com>
2022-01-03 06:25:35 -0300

    refactor: replace elment Formik by useFormik

--- src/pages/Login/Register.tsx
+++ src/pages/Login/Register.tsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
-import { Formik, FormikProps } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 import MyLink from '../../components/Link';
@@ -14,132 +14,127 @@ interface FormValues {
   email: string;
   password: string;
   passwordConfirm: string;
+  submit?: {
+    message: string;
+  };
 }
 
 const Register: React.FC = () => {
-  const [errorMessage, setErrorMessage] = React.useState('');
   const errorMessageRef = React.useRef<HTMLElement | null>(null);
 
-  const initialValues: FormValues = {
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirm: '',
-  };
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().max(255).required('Please enter you name full'),
+      email: Yup.string()
+        .email('Please enter a valid email address.')
+        .max(255)
+        .required('Please inform the email'),
+      password: Yup.string()
+        .min(8)
+        .max(25)
+        .required('Please enter the password'),
+      passwordConfirm: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Please password confirm'),
+    }),
+    onSubmit: (values, { setStatus, setSubmitting, setErrors }) => {
+      const message = 'Ops! We cannot save any data as it is a demo site.';
+      errorMessageRef.current?.scrollIntoView();
 
-  const validationSchema: Yup.SchemaOf<FormValues> = Yup.object({
-    name: Yup.string().max(255).required('Please enter you name full'),
-    email: Yup.string()
-      .email('Please enter a valid email address.')
-      .max(255)
-      .required('Please inform the email'),
-    password: Yup.string().min(8).max(25).required('Please enter the password'),
-    passwordConfirm: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
-      .required('Please password confirm'),
+      setErrors({ submit: message });
+      setStatus({ success: false });
+      setSubmitting(false);
+    },
   });
 
   return (
     <>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setStatus, setSubmitting }) => {
-          setErrorMessage('Ops! We cannot save any data as it is a demo site.');
-          setStatus({ success: false });
-          setSubmitting(false);
-
-          errorMessageRef.current?.scrollIntoView();
-        }}>
-        {({
-          errors,
-          handleChange,
-          handleSubmit,
-          isSubmitting,
-          values,
-        }: FormikProps<FormValues>) => (
-          <form noValidate onSubmit={handleSubmit}>
-            {errorMessage && (
-              <Typography ref={errorMessageRef} gutterBottom color="error.main">
-                {errorMessage}
-              </Typography>
-            )}
-            <TextField
-              id="name"
-              name="name"
-              label="Name"
-              variant="outlined"
-              fullWidth
-              autoFocus
-              required
-              type="text"
-              margin="normal"
-              value={values.name}
-              onChange={handleChange}
-              error={!!errors.name}
-              helperText={errors.name}
-            />
-
-            <TextField
-              id="email"
-              name="email"
-              autoComplete="email"
-              label="Email"
-              variant="outlined"
-              fullWidth
-              required
-              type="email"
-              margin="normal"
-              value={values.email}
-              onChange={handleChange}
-              error={!!errors.email}
-              helperText={errors.email}
-            />
-
-            <TextField
-              id="password"
-              name="password"
-              label="Password"
-              autoComplete="password"
-              variant="outlined"
-              fullWidth
-              required
-              type="password"
-              margin="normal"
-              value={values.password}
-              onChange={handleChange}
-              error={!!errors.password}
-              helperText={errors.password}
-            />
-
-            <TextField
-              id="passwordConfirm"
-              name="passwordConfirm"
-              label="Confirm Password"
-              variant="outlined"
-              fullWidth
-              required
-              type="password"
-              margin="normal"
-              value={values.passwordConfirm}
-              onChange={handleChange}
-              error={!!errors.passwordConfirm}
-              helperText={errors.passwordConfirm}
-            />
-
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              type="submit"
-              size="large"
-              disabled={isSubmitting}
-              sx={{ marginY: 3 }}>
-              {isSubmitting ? 'Loading ...' : 'Register'}
-            </Button>
-          </form>
+      <form noValidate onSubmit={formik.handleSubmit}>
+        {formik.errors.submit && (
+          <Typography ref={errorMessageRef} gutterBottom color="error.main">
+            {formik.errors.submit}
+          </Typography>
         )}
-      </Formik>
+        <TextField
+          id="name"
+          name="name"
+          label="Name"
+          variant="outlined"
+          fullWidth
+          autoFocus
+          required
+          type="text"
+          margin="normal"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={!!formik.errors.name}
+          helperText={formik.errors.name}
+        />
+
+        <TextField
+          id="email"
+          name="email"
+          autoComplete="email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          required
+          type="email"
+          margin="normal"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={!!formik.errors.email}
+          helperText={formik.errors.email}
+        />
+
+        <TextField
+          id="password"
+          name="password"
+          label="Password"
+          autoComplete="password"
+          variant="outlined"
+          fullWidth
+          required
+          type="password"
+          margin="normal"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={!!formik.errors.password}
+          helperText={formik.errors.password}
+        />
+
+        <TextField
+          id="passwordConfirm"
+          name="passwordConfirm"
+          label="Confirm Password"
+          variant="outlined"
+          fullWidth
+          required
+          type="password"
+          margin="normal"
+          value={formik.values.passwordConfirm}
+          onChange={formik.handleChange}
+          error={!!formik.errors.passwordConfirm}
+          helperText={formik.errors.passwordConfirm}
+        />
+
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          type="submit"
+          size="large"
+          disabled={formik.isSubmitting}
+          sx={{ marginY: 3 }}>
+          {formik.isSubmitting ? 'Loading ...' : 'Register'}
+        </Button>
+      </form>
 
       <MyLink to="/login">Back to Login</MyLink>
     </>
--- src/pages/Login/SignIn.tsx
+++ src/pages/Login/SignIn.tsx
@@ -8,7 +8,7 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 
-import { Formik, FormikProps } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 import MyLink from '../../components/Link';
@@ -18,113 +18,100 @@ import { isRequestError } from '../../utils/request';
 interface FormValues {
   email: string;
   password: string;
+  submit?: {
+    message: string;
+  };
 }
 
 const SignIn: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [errorMessage, setErrorMessage] = React.useState('');
   const errorMessageRef = React.useRef<HTMLElement | null>(null);
 
-  const initialValues: FormValues = {
-    email: 'john@example.com',
-    password: 'password@123',
-  };
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      email: 'john@example.com',
+      password: 'password@123',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Please enter a valid email address.')
+        .max(255)
+        .required('Please inform the email'),
+      password: Yup.string().max(25).required('Please enter the password'),
+    }),
+    onSubmit: async (values, { setStatus, setSubmitting, setErrors }) => {
+      try {
+        await dispatch(accountAction.signIn(values.email, values.password));
+        navigate('/');
+      } catch (error: any) {
+        const message = isRequestError(error)
+          ? error.response.data.message
+          : error.message;
 
-  const validationSchema: Yup.SchemaOf<FormValues> = Yup.object({
-    email: Yup.string()
-      .email('Please enter a valid email address.')
-      .max(255)
-      .required('Please inform the email'),
-    password: Yup.string().max(25).required('Please enter the password'),
+        setErrors({ submit: message });
+        setStatus({ success: false });
+        setSubmitting(false);
+      }
+    },
   });
 
   return (
     <>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={async (values, { setStatus, setSubmitting }) => {
-          try {
-            await dispatch(accountAction.signIn(values.email, values.password));
-            navigate('/');
-          } catch (error: any) {
-            const message = isRequestError(error)
-              ? error.response.data.message
-              : error.message;
-
-            errorMessageRef.current?.scrollIntoView();
-
-            setErrorMessage(message);
-            setStatus({ success: false });
-            setSubmitting(false);
-          }
-        }}>
-        {({
-          errors,
-          handleChange,
-          handleSubmit,
-          isSubmitting,
-          values,
-        }: FormikProps<FormValues>) => (
-          <form noValidate onSubmit={handleSubmit}>
-            {errorMessage && (
-              <Typography ref={errorMessageRef} gutterBottom color="error.main">
-                {errorMessage}
-              </Typography>
-            )}
-            <TextField
-              id="email"
-              name="email"
-              autoComplete="email"
-              label="Email"
-              variant="outlined"
-              fullWidth
-              autoFocus
-              type="email"
-              margin="normal"
-              error={!!errors.email}
-              helperText={errors.email}
-              value={values.email}
-              onChange={handleChange}
-            />
-
-            <TextField
-              id="password"
-              name="password"
-              label="Password"
-              autoComplete="password"
-              variant="outlined"
-              fullWidth
-              type="password"
-              margin="normal"
-              error={!!errors.password}
-              helperText={errors.password}
-              value={values.password}
-              onChange={handleChange}
-            />
-
-            <FormControlLabel
-              control={
-                <Checkbox defaultChecked id="remember" name="remember" />
-              }
-              label="Remember me"
-            />
-
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              type="submit"
-              size="large"
-              disabled={isSubmitting}
-              sx={{ marginY: 3 }}>
-              {isSubmitting ? 'Loading ...' : 'Sign In'}
-            </Button>
-          </form>
+      <form noValidate onSubmit={formik.handleSubmit}>
+        {formik.errors.submit && (
+          <Typography ref={errorMessageRef} gutterBottom color="error.main">
+            {formik.errors.submit}
+          </Typography>
         )}
-      </Formik>
+        <TextField
+          id="email"
+          name="email"
+          autoComplete="email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          autoFocus
+          type="email"
+          margin="normal"
+          error={!!formik.errors.email}
+          helperText={formik.errors.email}
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+
+        <TextField
+          id="password"
+          name="password"
+          label="Password"
+          autoComplete="password"
+          variant="outlined"
+          fullWidth
+          type="password"
+          margin="normal"
+          error={!!formik.errors.password}
+          helperText={formik.errors.password}
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+
+        <FormControlLabel
+          control={<Checkbox defaultChecked id="remember" name="remember" />}
+          label="Remember me"
+        />
+
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          type="submit"
+          size="large"
+          disabled={formik.isSubmitting}
+          sx={{ marginY: 3 }}>
+          {formik.isSubmitting ? 'Loading ...' : 'Sign In'}
+        </Button>
+      </form>
 
       <MyLink to="/login/register">Create a account</MyLink>
     </>
